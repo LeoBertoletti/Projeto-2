9217d432cd95744505fa7a1318ecdd424bb7a503
jrafaelbdo <130498382+jrafaelbdo@users.noreply.github.com>
2023-05-04 16:19:59 -0300

    ClimApp 2

--- daysInformation.js
+++ daysInformation.js
@@ -1,45 +1,96 @@
   
   function getWeather(lat, lon) {
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=-30.03&longitude=-51.23&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timeformat=unixtime&past_days=92&forecast_days=16&timezone=auto`;
-    alert(url);
-  
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,windspeed_10m_max,apparent_temperature_max&current_weather=true&timeformat=unixtime&past_days=92&forecast_days=16&timezone=auto`;
+    alert(url)
+
     fetch(url)
       .then(response => response.json())
       .then(data => {
         const weatherInfo = document.getElementById("weather-info");
-        const elevation = data.elevation;
-  
-        const days = ["Ontem-7", "Ontem-6", "Ontem-5", "Ontem-4", "Ontem-3", "Ontem-2", "Ontem-1", "Hoje", "Amanhã", "Depois de amanhã", "Em 3 dias", "Em 4 dias", "Em 5 dias", "Em 6 dias", "Em 7 dias"];
-  
+        const currentTemp = data.current_weather.temperature;
+        const days = data.daily.time
+        var c = 0;
+    
         let html = "";
-        for (let i = 85; i <= 99; i++) {
-          const minTemp = data.daily.temperature_2m_min[i];
-          const maxTemp = data.daily.temperature_2m_max[i];
-          const weatherCode = data.daily.weathercode[i];
+        days.forEach(element => {
+          const minTemp = data.daily.temperature_2m_min[c];
+          const maxTemp = data.daily.temperature_2m_max[c];
+          const weatherCode = data.daily.weathercode[c];
           const weatherDescription = getWeatherDescription(weatherCode);
-          const day = days[i -85];
-          const sunrise = data.daily.sunrise[i];
-          const sunset = data.daily.sunset[i];
+          const windspeed = data.daily.windspeed_10m_max[c];
+          const apparentTemp = data.daily.apparent_temperature_max[c];
+
+          // Get todayName:
+
+          const timestampToday = data.current_weather.time;
+          const dateToday = new Date (timestampToday * 1000);
+          const todayName = dateToday.toLocaleDateString('pt-BR', { weekday: 'long' });
+
+          // Get dayName:
+
+          const timestampDayName = data.daily.time[c];
+          const dateDayName = new Date(timestampDayName * 1000);
+          const dayOfWeek = dateDayName.toLocaleDateString('pt-BR', { weekday: 'long' });
+
+          // Get Sunrise and Sunset Hours:
+          
+          const timestampSunrise =  data.daily.sunrise[c];
+          const timestampSunset = data.daily.sunset[c];
+          const dateSunrise = new Date(timestampSunrise * 1000); 
+          const dateSunset = new Date(timestampSunset * 1000);
+          const hoursSunrise = dateSunrise.getHours().toString().padStart(2, '0');
+          const hoursSunset = dateSunset.getHours().toString().padStart(2, '0');
+          const minutesSunrise = dateSunrise.getMinutes().toString().padStart(2, '0');
+          const minutesSunset= dateSunset.getMinutes().toString().padStart(2, '0');
+          const formattedHourSunrise = `${hoursSunrise}:${minutesSunrise}`;
+          const formattedHourSunset =  `${hoursSunset}:${minutesSunset}`;
+
+
   
-          if (day === days[7]) {
-            html += `<h1>${day}:</h1>
-            <p>Temperatura Atual: ${elevation}ºC;</p>
+          if (c === 91) {
+            html += `<h1>Ontem:</h1>
             <p>Temperatura Mínima: ${minTemp}ºC;</p>
             <p>Temperatura Máxima: ${maxTemp}ºC;</p>
+            <p>Sensação Térmica: ${apparentTemp}ºC;</p>
             <p>Descrição do Tempo: ${weatherDescription}</p>
-            <p>Nascer do Sol: ${sunrise};</p>
-            <p>Por do Sol: ${sunset}</p>`;
+            <p>Nascer do Sol: ${formattedHourSunrise};</p>
+            <p>Por do Sol: ${formattedHourSunset}</p>
+            <p>Vento: ${windspeed}Km/h</p>`;
             
           }
-          else{
-            html += `<h1>${day}:</h1>
+          else if (c === 92){
+            html += `<h1>Hoje:</h1>
+          <p>Temperatura Atual: ${currentTemp}ºC;</p>
           <p>Temperatura Mínima: ${minTemp}ºC;</p>
           <p>Temperatura Máxima: ${maxTemp}ºC;</p>
+          <p>Sensação Térmica: ${apparentTemp}ºC;</p>
           <p>Descrição do Tempo: ${weatherDescription}</p>
-          <p>Nascer do Sol: ${sunrise};</p>
-          <p>Por do Sol: ${sunset}</p>`;
+          <p>Nascer do Sol: ${formattedHourSunrise}</p>
+          <p>Por do Sol: ${formattedHourSunset}</p>
+          <p>Vento: ${windspeed}Km/h</p>`;
           }
-        }
+          else if (c === 93){
+            html += `<h1>Amanhã:</h1>
+          <p>Temperatura Mínima: ${minTemp}ºC;</p>
+          <p>Temperatura Máxima: ${maxTemp}ºC;</p>
+          <p>Sensação Térmica: ${apparentTemp}ºC;</p>
+          <p>Descrição do Tempo: ${weatherDescription}</p>
+          <p>Nascer do Sol: ${formattedHourSunrise}</p>
+          <p>Por do Sol: ${formattedHourSunset}</p>
+          <p>Vento: ${windspeed}Km/h</p>`;
+          }
+          else{
+            html += `<h1>${dayOfWeek}:</h1>
+          <p>Temperatura Mínima: ${minTemp}ºC;</p>
+          <p>Temperatura Máxima: ${maxTemp}ºC;</p>
+          <p>Sensação Térmica: ${apparentTemp}ºC;</p>
+          <p>Descrição do Tempo: ${weatherDescription}</p>
+          <p>Nascer do Sol: ${formattedHourSunrise}</p>
+          <p>Por do Sol: ${formattedHourSunset}</p>
+          <p>Vento: ${windspeed}Km/h</p>`;
+          }
+          c++;
+        });
   
         weatherInfo.innerHTML = html;
       })
