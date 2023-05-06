68370a7e3f8896d2b601306fdfa4f4ca221c9326
LeoBertoletti <Leonardo.b@edu.pucrs.br>
2023-05-06 00:46:11 -0300

    js

--- getLocationAuto.js
+++ getLocationAuto.js
@@ -2,9 +2,16 @@ function getLocationAuto() {
     if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition(
             (position) => {
-                console.log(position)
                 const lat = position.coords.latitude;
                 const lng = position.coords.longitude;
+
+                fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json&zoom=18`)
+                    .then(response => response.json())
+                    .then(dataLocal => {
+                        document.getElementById("main_local").innerText = dataLocal.address.city
+                    })
+                    .catch(error => alert("Cidade não existe"));
+
                 getWeather(lat, lng)
             },
             (error) => {
